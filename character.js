4347f020cf7ebe7f49b868ac3415930561cb28cf
Mario Leone <mleone10@gmail.com>
2020-04-08 21:40:28 -0400

    Adding character class

---
+++ character.js
@@ -0,0 +1,19 @@
+class Character {
+    constructor() {
+    }
+
+    setAbilities(abilities) {
+        this.abilities = abilities
+    }
+}
+
+class Abilities {
+    constructor(str, con, dex, wis, intel, cha) {
+        this.str = str
+        this.con = con
+        this.dex = dex
+        this.wis = wis
+        this.intel = intel
+        this.cha = cha
+    }
+}
--- db.js
+++ db.js
@@ -1,14 +1,5 @@
 var db = {
     getCharacter: function() {
-        return {
-            "abilities": {
-                "strength": 12,
-                "constitution": 10,
-                "dexterity": 12,
-                "wisdom": 14,
-                "intelligence": 13,
-                "charisma": 8
-            }
-        }
+        return new Character(new Abilities(12, 10,12, 14, 13, 8))
     }
 }
