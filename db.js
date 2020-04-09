95d1418e54214bf3714dd668a511b568d0e4acb4
Mario Leone <mleone10@gmail.com>
2020-04-08 21:31:02 -0400

    Example db module

---
+++ db.js
@@ -0,0 +1,14 @@
+var db = {
+    getCharacter: function() {
+        return {
+            "abilities": {
+                "strength": 12,
+                "constitution": 10,
+                "dexterity": 12,
+                "wisdom": 14,
+                "intelligence": 13,
+                "charisma": 8
+            }
+        }
+    }
+}
