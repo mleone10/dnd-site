b77b3fa7b05d5ba3b281293d971c26ee25151a0e
Mario Leone <mleone10@gmail.com>
2020-04-08 22:21:03 -0400

    Fixing db class

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 import {Character, Abilities} from './character.js'
 
 export class Database {
+    constructor() {
+    }
+
     getCharacter() {
         return new Character(
             new Abilities(12, 10,12, 14, 13, 8)
