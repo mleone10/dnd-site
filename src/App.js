bb1f84f23932aab0b88aef93bc563897f757d13a
Mario Leone <mleone10@gmail.com>
2020-07-17 18:43:56 -0400

    Restarting migration.  Starting from a working site and moving plain HTML pieces into React gradually.  Migrating name and flavor text.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import FlavorText from './FlavorText';
-import MultiStatBlock from './MultiStatBlock';
+import React from "react";
 
-class App extends React.Component  {
-    render() {
-        return (
-            <div className="App">
-            <h1>Letheryl</h1>
-            <FlavorText text={this.props.characterDetails.flavortext} />
-            <MultiStatBlock details={this.props.characterDetails} />
-            </div>
-        );
-    }
+const FLAVOR_TEXT =
+  "\"No, I can't say this is exactly my 'comfort zone' - we departed <b>that</b> happy Grove quite some time ago. But I suppose it's all fallen on me, hmm? 'An elf bleeds the same as anyone,' as they say, so I've got quite a weight on my shoulders...\"";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <CharacterName name="Letheryl" />
+        <FlavorText content={getFlavorText()} />
+      </div>
+    );
+  }
+}
+
+function CharacterName(props) {
+  return <h1>{props.name}</h1>;
+}
+
+function FlavorText(props) {
+  return (
+    <div id="flavortext">
+      <p dangerouslySetInnerHTML={{ __html: FLAVOR_TEXT }} />
+    </div>
+  );
 }
 
 export default App;
