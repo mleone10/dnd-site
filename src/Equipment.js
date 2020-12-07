fc3c704a46ea9e7a371bcbff019426f93bc8c6a6
Mario Leone <mleone10@gmail.com>
2020-12-06 19:31:44 -0500

    Move Equipment to React component using fetched JSON

--- src/Equipment.js
+++ src/Equipment.js
@@ -2,23 +2,55 @@ import React from "react";
 
 class Equipment extends React.Component {
   state = {
-    equipment: [],
+    uncategorized: [],
+    categories: [],
   };
 
   componentDidMount() {
     // TODO: Retrieve equipment from API
     fetch(process.env.PUBLIC_URL + "/data/equipment.json")
       .then((res) => res.json())
-      .then((data) => this.setState({ features: data.features }));
+      .then((data) =>
+        this.setState({
+          uncategorized: data.uncategorized,
+          categories: data.categories,
+        })
+      );
   }
 
   render() {
+    if (
+      !this.state.uncategorized ||
+      Object.entries(this.state.uncategorized) === 0
+    )
+      return <div></div>;
     return (
       <div id="equipment">
         <h3>Equipment</h3>
+        <ul>
+          {this.state.uncategorized.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+          {this.state.categories.map((category) => (
+            <Category category={category} key={category.name} />
+          ))}
+        </ul>
       </div>
     );
   }
+}
+
+function Category(props) {
+  return (
+    <li>
+      <b>{props.category.name}</b>
+      <ul>
+        {props.category.items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </li>
+  );
 }
 
 export default Equipment;
