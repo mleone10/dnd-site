ccb0a253d8acff81b4a8ce00151829152ae0fcef
Mario Leone <mleone10@gmail.com>
2020-12-06 19:42:10 -0500

    Minor formatting of Spells component

--- src/Spells.js
+++ src/Spells.js
@@ -4,22 +4,25 @@ class Spells extends React.Component {
   // TODO: Populate list of spells range, descriptions, etc from an API
   state = {
     spells: {},
+    spellSlots: 0,
   };
 
   componentDidMount() {
     // TODO: Retrieve spells from API
     fetch(process.env.PUBLIC_URL + "/data/spells.json")
       .then((res) => res.json())
-      .then((data) => this.setState({ spells: data.spells, spellSlots: data.spellSlots }));
+      .then((data) =>
+        this.setState({ spells: data.spells, spellSlots: data.spellSlots })
+      );
   }
 
   render() {
+    if (!this.state.spells || Object.entries(this.state.spells).length === 0)
+      return <div></div>;
     return (
       <div id="spells">
         <h3>Spells</h3>
-        <i>
-          {this.state.spellSlots} spell slots, regained on short rest
-        </i>
+        <i>{this.state.spellSlots} spell slots, regained on short rest</i>
         <SpellsList spells={this.state.spells} />
       </div>
     );
