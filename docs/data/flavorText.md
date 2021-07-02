7733505489c119be11345c9cc45d7bf7de11df0b
mleone10 <mleone10@gmail.com>
2021-07-02 01:27:17 +0000

    Updating site

---
+++ docs/data/flavorText.md
@@ -0,0 +1,1 @@
+"No, I can't say this is exactly my 'comfort zone' - we departed **that** happy Grove quite some time ago. But I suppose it's all fallen on me, hmm? 'An elf bleeds the same as anyone,' as they say, so I've got quite a weight on my shoulders..."
---
+++ docs/precache-manifest.9f81d0d3002ab8b9bd40aa810ff7eaa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26431ac2e34edf885935bb7aa963a81f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d4ed0ff3526020f9892",
+    "url": "/static/js/2.e53f2c30.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.e53f2c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a82ce7100e7008fa10",
+    "url": "/static/js/main.3e60a458.chunk.js"
+  },
+  {
+    "revision": "32b5ca4624306c683254",
+    "url": "/static/js/runtime-main.cd6ea54c.js"
+  }
+]);
